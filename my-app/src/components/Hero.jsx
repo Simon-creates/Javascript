caab890b7661201c81e65f16aa829c28be279867
Simon Nilsson <s.i.m.o.n89.sn@gmail.com>
2025-11-05 11:39:18 +0100

    8th commit. CSS för Navbar påbörjad.

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -1,7 +1,18 @@
 import { Link } from 'react-router-dom';
+import '../App.css';
 
 function Hero() {
-  return <div style={{ background: 'lightgray', padding: '1rem' }}>Hero syns!</div>;
+  return (
+    <div className="hero">
+      <h1>H1 text</h1>
+      
+      <Link to="/projects" className="hero-button">
+        Knapp/länk
+      </Link>
+    </div>
+
+ ); 
+  
 }
 
 export default Hero;
