3634398e6fde33f0dae459843a81fbcc63167035
Simon Nilsson <s.i.m.o.n89.sn@gmail.com>
2025-11-12 13:01:24 +0100

    17th commit. API-påbörjat

--- my-app/src/components/pages/Home.jsx
+++ my-app/src/components/pages/Home.jsx
@@ -2,14 +2,37 @@ import Hero from "../sections/Hero";
 import About from "../sections/About";
 import Logos from "../sections/Logos";
 import Services from "../sections/Services";
+import Subscribe from "../sections/Subscribe";
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const baseUrl = import.meta.env.VITE_API_URL;
+    
+
+    fetch(`${baseUrl}/api/subscribe`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email: "user@example.com" }) 
+    })
+      .then(res => {
+        if (!res.ok) throw new Error("API svarade inte");
+        return res.json();
+      })
+      .then(data => console.log("Svar från /api/subscribe:", data))
+      .catch(err => console.error("Fel vid API-anrop:", err));
+  }, []);
+
+
   return (
-    <>
+    <section>
       <Hero />
       <About />
       <Logos />
       <Services />
-    </>
+      <Subscribe />
+    </section>
   );
 }
---
+++ my-app/src/components/sections/Subscribe.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+import './Subscribe.css';
+
+
+export default function Subscribe() {
+  return (
+      <div>Subscribe text</div>
+  );
+}
